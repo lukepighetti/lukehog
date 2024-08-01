3ca15a2c28f6e5ce922ba6cf5036348a458668b7
Luke Pighetti <lukepighetti@gmail.com>
2024-08-01 11:27:11 -0400

    lukehog v0.4.0

---
+++ lukehog/CHANGELOG.md
@@ -0,0 +1,21 @@
+## 0.4.0
+
+- migrate to a flutter only package, use `lukehog_client` for pure dart projects
+
+## 0.3.0
+
+- add storage
+
+## 0.2.1
+
+- add Lukehog.useAnonymousUserId()
+- fix parsing error
+- remove auto retry
+
+## 0.2.0
+
+- Make appId constructor argument positional
+
+## 0.1.0
+
+- Initial version.
---
+++ lukehog/lib/lukehog.dart
@@ -0,0 +1,79 @@
+library lukehog;
+
+import 'package:flutter/foundation.dart';
+import 'package:lukehog_client/lukehog_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Dead simple analytics for any Flutter platform
+///
+/// ```dart
+/// final analytics = Lukehog("<appId>");
+/// analytics.capture("test_event");
+/// ```
+class Lukehog {
+  /// Dead simple analytics for any Flutter platform
+  ///
+  /// ```dart
+  /// final analytics = Lukehog("<appId>");
+  /// analytics.capture("test_event");
+  /// ```
+  Lukehog(
+    this.appId, {
+    this.sessionExpiration = const Duration(minutes: 15),
+    this.baseUrl = 'https://api.lukehog.com',
+    this.debug = kDebugMode,
+    this.sharedPreferences,
+  });
+
+  /// The appId to save events to
+  final String appId;
+
+  /// Create a new sessionId after not sending events for this duration
+  final Duration sessionExpiration;
+
+  /// The location of the Lukehog server
+  final String baseUrl;
+
+  /// Debug events are automatically omitted from reports, but are recorded to your database
+  final bool debug;
+
+  late final _client = LukehogClient(
+    appId,
+    sessionExpiration: sessionExpiration,
+    baseUrl: baseUrl,
+    debug: debug,
+    setString: (key, value) async {
+      await _prefs().then((x) => x.setString(key, value));
+    },
+    getString: (key) async {
+      return _prefs().then((x) => x.getString(key));
+    },
+  );
+
+  /// Provide your own shared preferences instance if you desire
+  SharedPreferences? sharedPreferences;
+
+  Future<SharedPreferences> _prefs() async {
+    return sharedPreferences ??= await SharedPreferences.getInstance();
+  }
+
+  /// Set the current userId. Set it to null to roll a new anonymous userId
+  void setUserId(String? userId) async {
+    _client.setUserId(userId);
+  }
+
+  /// Send an event to the Lukehog servers.
+  ///
+  /// Currently uses a best effort online only strategy.
+  Future<void> capture(
+    String event, {
+    Map<String, dynamic> properties = const {},
+    DateTime? timestamp,
+  }) async {
+    await _client.capture(
+      event,
+      properties: properties,
+      timestamp: timestamp,
+    );
+  }
+}
