e66a5dd1847e6c8db5591029e502f850b44b57d6
Luke Pighetti <lukepighetti@gmail.com>
2024-08-09 14:27:23 -0400

    quick fix vertical scrolling

--- ffa_app/lib/screens/home_screen.dart
+++ ffa_app/lib/screens/home_screen.dart
@@ -85,43 +85,46 @@ class _HomeScreenState extends State<HomeScreen> {
               )
             else
               SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  dataTextStyle: context.textBody,
-                  headingTextStyle: context.textCaption,
-                  columnSpacing: 24,
-                  columns: [
-                    DataColumn(label: Text("Day")),
-                    for (final x
-                        in state.visibleAppEvents.first.columns.entries)
-                      DataColumn(
-                        label: Flexible(
-                          child: Text(
-                            switch (x.key) {
-                              "unique_sessions" => "Sessions",
-                              "unique_users" => "Users",
-                              String x => x,
-                            },
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
+                scrollDirection: Axis.vertical,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    dataTextStyle: context.textBody,
+                    headingTextStyle: context.textCaption,
+                    columnSpacing: 24,
+                    columns: [
+                      DataColumn(label: Text("Day")),
+                      for (final x
+                          in state.visibleAppEvents.first.columns.entries)
+                        DataColumn(
+                          label: Flexible(
+                            child: Text(
+                              switch (x.key) {
+                                "unique_sessions" => "Sessions",
+                                "unique_users" => "Users",
+                                String x => x,
+                              },
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ),
-                      ),
-                  ],
-                  rows: [
-                    for (final row in state.visibleAppEvents)
-                      DataRow(
-                        cells: [
-                          DataCell(
-                            Text(row.day.toYYYYMMDDString()),
-                          ),
-                          for (final cell in row.columns.entries)
+                    ],
+                    rows: [
+                      for (final row in state.visibleAppEvents)
+                        DataRow(
+                          cells: [
                             DataCell(
-                              Text(cell.value.toString()),
+                              Text(row.day.toYYYYMMDDString()),
                             ),
-                        ],
-                      ),
-                  ],
+                            for (final cell in row.columns.entries)
+                              DataCell(
+                                Text(cell.value.toString()),
+                              ),
+                          ],
+                        ),
+                    ],
+                  ),
                 ),
               ),
           ],
